07d24faa67c2f4d65ac8d2eac0cb9335469c6843
brook-bang <guozijian8976584@gmail.com>
2025-03-22 17:12:22 +0800

    start chat

---
+++ chat/src/main.rs
@@ -0,0 +1,92 @@
+#![warn(rust_2018_idioms)]
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::{Mutex, mpsc};
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, LinesCodec};
+
+use futures::SinkExt;
+use std::collections::HashMap;
+use std::env;
+use std::error::Error;
+use std::io;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<Box<dyn Error>> {
+    use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan};
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive("chat=info".parse()?))
+        .with_span_events(FmtSpan::FULL)
+        .init();
+
+    let state = Arc::new(Mutex::new(Shared::new()));
+
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:6142".to_string());
+
+    let listener = TcpListener::bind(&addr).await?;
+    tracing::info!("server running on {}", addr);
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        let state = Arc::clone(&state);
+        tokio::spawn(async move {
+            tracing::debug!("accepted connection");
+            if let Err(e) = process(state, stream, addr).await {
+                tracing::info!("an error occurred; error = {:?}", e);
+            }
+        });
+    }
+}
+
+type Tx = mpsc::UnboundedSender<String>;
+
+type Rx = mpsc::UnboundedReceiver<String>;
+
+struct Shared {
+    peers: HashMap<SocketAddr, Tx>,
+}
+
+struct Peer {
+    lines: Framed<TcpStream, LinesCodec>,
+    rx: Rx,
+}
+
+impl Shared {
+    fn new() -> Self {
+        Shared {
+            peers: HashMap::new(),
+        }
+    }
+
+    async fn broadcast(&mut self, sender: SocketAddr, message: &str) {
+        for peer in self.peers.iter_mut() {
+            if *peer.0 != sender {
+                let _ = peer.1.send(message.into());
+            }
+        }
+    }
+}
+
+impl Peer {
+    async fn new(
+        state: Arc<Mutex<Shared>>,
+        lines: Framed<TcpStream, LinesCodec>,
+    ) -> io::Result<Peer> {
+        let addr = lines.get_ref().peer_addr()?;
+        let (tx, rx) = mpsc::unbounded_channel();
+        state.lock().await.peers.insert(addr, tx);
+        Ok(Peer { lines, rx })
+    }
+}
+
+async fn process(
+    state: Arc<Mutex<Shared>>,
+    stream: TcpStream,
+    addr: SocketAddr,
+) -> Result<(),Box<dyn Error>> {
+    let mut lines = Framed::new(inner, codec)
+
+}
